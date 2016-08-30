a4274b338bedf7647ad57df6391e9f05e960035b
Dmitry Mikhaylovich <dmitry.mikhailovich@gmail.com>
2016-08-30 18:46:44 +0300

    add spring boot basic

---
+++ src/main/java/com/mikhaylovich/test/Application.java
@@ -0,0 +1,15 @@
+package com.mikhaylovich.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dmitry Mikhaylovich
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
